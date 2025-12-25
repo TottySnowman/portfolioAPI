e3d56b10c478a8281330252cb0b2f6a6311b9a61
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2025-12-26 00:45:12 +0100

    add cors for ws connection

--- chat/services/wsService.go
+++ chat/services/wsService.go
@@ -1,25 +1,29 @@
 package chatService
 
 import (
+	"os"
+
 	"github.com/coder/websocket"
 	"github.com/gin-gonic/gin"
 )
 
-type WsService struct {}
+type WsService struct{}
 
 func NewWsService() *WsService {
 	return &WsService{}
 }
 
-func(service *WsService) GetWebsocketConnection(ctx *gin.Context)*websocket.Conn{
-
+func (service *WsService) GetWebsocketConnection(ctx *gin.Context) *websocket.Conn {
+  ws_cors0 := os.Getenv("WS_CORS_ORIGIN0")
+  ws_cors1 := os.Getenv("WS_CORS_ORIGIN1")
 	acceptOptions := websocket.AcceptOptions{
-		OriginPatterns: []string{"*"},
+    OriginPatterns: []string{ws_cors0, ws_cors1},
 	}
+
 	conn, err := websocket.Accept(ctx.Writer, ctx.Request, &acceptOptions)
 	if err != nil {
 		return nil
 	}
 
-  return conn
+	return conn
 }
